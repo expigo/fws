e3df44413b22883d7daea5196b95667d7156f9c8
Kryspin Musiol <kryspin.musiol@gmail.com>
2019-04-16 17:45:23 +0200

    Get info about movies from the top500 list's first result page

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
@@ -44,8 +44,9 @@ func main() {
 	}
 
 	// data.Add("_login_redirect_url", "https://www.filmweb.pl/user/"+*u)
-	data.Add("_login_redirect_url", "https://www.filmweb.pl/film/Nietykalni-2011-583390")
+	// data.Add("_login_redirect_url", "https://www.filmweb.pl/film/Nietykalni-2011-583390")
 	// data.Add("_login_redirect_url", "https://www.filmweb.pl/Skazani.Na.Shawshank")
+	data.Add("_login_redirect_url", "https://www.filmweb.pl/ranking/film")
 
 	req, err := http.NewRequest("POST", "https://www.filmweb.pl/j_login", strings.NewReader(data.Encode()))
 	if err != nil {
@@ -68,20 +69,54 @@ func main() {
 	defer out.Close()
 
 	doc, _ := goquery.NewDocumentFromResponse(resp)
-	htmlContent, _ := doc.Html()
-	io.Copy(out, strings.NewReader(htmlContent))
+	// doc.Find(".film").EachWithBreak(func(i int, s *goquery.Selection) bool {
+	// 	if i == 100 {
+	// 		return false
+	// 	} else {
+	// 		fmt.Println(s.Text())
+	// 		return true
+	// 	}
+	// })
+	// htmlContent, _ := doc.Html()
+	// io.Copy(out, strings.NewReader(htmlContent))
 
-	//  , ],{avg: 9.17, count: 43, limit: 4}] })
-	userRating := regexp.MustCompile(`],{avg: (.*?), count: (.*?),`)
-	userRatingRaw := userRating.FindStringSubmatch(htmlContent)
-	fmt.Println(userRatingRaw[1])
-	fmt.Println(userRatingRaw[2])
-
-	communityRating := regexp.MustCompile(`communityRateInfo:"(.*?)",communityRatingCountInfo:"(.*?) ocen."`)
-	communityRatingRaw := communityRating.FindStringSubmatch(htmlContent)
-	fmt.Println(communityRatingRaw[1])
-	fmt.Println(communityRatingRaw[2])
+	doc.Find(".filmPoster__filmLink").EachWithBreak(func(i int, s *goquery.Selection) bool {
+		if i == 100 {
+			return false
+		} else {
+			fmt.Println(s.Text())
+			href, exists := s.Attr("href")
+			if exists {
+				fmt.Println(href)
+				parse(href, &client)
+				fmt.Println("-----")
+			}
+			return true
+		}
+	})
 
 	fmt.Print("Done 👍")
 
+}
+
+func parse(url string, c *http.Client) {
+	//  , ],{avg: 9.17, count: 43, limit: 4}] })
+
+	resp, _ := c.Get("https://filmweb.pl" + url)
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		bodyString := string(bodyBytes)
+
+		userRating := regexp.MustCompile(`],{avg: (.*?), count: (.*?),`)
+		userRatingRaw := userRating.FindStringSubmatch(bodyString)
+		fmt.Printf("%T %[1]v\n", userRatingRaw[1])
+		fmt.Println(userRatingRaw[2])
+
+		communityRating := regexp.MustCompile(`communityRateInfo:"(.*?)",communityRatingCountInfo:"(.*?) ocen[y]?"`)
+		communityRatingRaw := communityRating.FindStringSubmatch(bodyString)
+		fmt.Println(communityRatingRaw[1])
+		fmt.Println(communityRatingRaw[2])
+	} else {
+		panic(":O")
+	}
 }
